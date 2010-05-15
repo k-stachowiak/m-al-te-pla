500143b052b055cbd5d92bb62aac085b4ad34771
krzysiek.stachowiak <krzysiek.stachowiak@d01adb59-8cad-fbe6-124c-f7b667d23f13>
2010-05-15 22:15:59 +0000

    Poprawka grupowania.

--- src/mpiech/grouping_functions.h
+++ src/mpiech/grouping_functions.h
@@ -5,18 +5,48 @@
 
 namespace grouping {
 
-int high_degree(network_abstraction& network, int multicast_nodes, std::vector<size_t>& result) {
+class distribution_record {
+public:
+	distribution_record(size_t new_graph_id) :
+		node_id(new_graph_id), neighbours_count(0) {
+	}
+
+	size_t get_node_id() const {
+		return node_id;
+	}
+
+	size_t get_neighbours_count() const {
+		return neighbours_count;
+	}
+
+	void add_neighbour() {
+		++neighbours_count;
+	}
+
+	bool operator<(const distribution_record& other) const {
+		return neighbours_count < other.neighbours_count;
+	}
+
+private:
+	size_t node_id;
+	size_t neighbours_count;
+};
+
+void high_degree(network_abstraction& network, int multicast_nodes,
+	std::vector<size_t>& result) {
 
 	size_t nodes_count = network.get_nodes_count();
-	std::vector<size_t> distribution(nodes_count, 0);
+	std::vector<distribution_record> distribution;
 
-	for(size_t from = 0; from < nodes_count; ++from) {
+	for (size_t from = 0; from < nodes_count; ++from) {
+		distribution_record new_record(from);
 		// Zał. że graf jest nieskierowany!
-		for(size_t to = (from+1); to < nodes_count; ++to) {
-			if(network.has_link(from, to)) {
-				++(distribution[from]);
+		for (size_t to = (from + 1); to < nodes_count; ++to) {
+			if (network.has_link(from, to)) {
+				new_record.add_neighbour();
 			}
 		}
+		distribution.push_back(new_record);
 	}
 
 	// To nie jest najlepsza decyzja projektowa, bo nie potrzeba sortować
@@ -26,8 +56,8 @@ int high_degree(network_abstraction& network, int multicast_nodes, std::vector<s
 	std::sort(distribution.begin(), distribution.end());
 
 	result.clear();
-	for(size_t i = 0; i < (multicast_nodes + 1); ++i) {
-		result.push_back(distribution[i]);
+	for (int i = 0; i < (multicast_nodes + 1); ++i) {
+		result.push_back(distribution[i].get_node_id());
 	}
 
 }
