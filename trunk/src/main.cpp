910618e28881eaf6730f3e98686982bf048423ad
krzysiek.stachowiak <krzysiek.stachowiak@d01adb59-8cad-fbe6-124c-f7b667d23f13>
2010-05-16 19:22:55 +0000

    Parametryzacja procedury liczącej.

--- trunk/src/main.cpp
+++ trunk/src/main.cpp
@@ -1,26 +1,34 @@
 #include <iostream>
 #include <fstream>
 
+#include <FastDelegate.h>
+
 #include "krzysiek/results_container.h"
 #include "krzysiek/network_abstraction.h"
 #include "krzysiek/network_helper.h"
+
+typedef fastdelegate::FastDelegate3<network_abstraction&, int, std::vector<
+	size_t>& > grouping_func_t;
+
+typedef fastdelegate::FastDelegate4<network_abstraction&, std::vector<size_t>&,
+	int, network_path&, int> algorithm_func_t;
 
 #include "mpiech/kpp_hash_map.h"
 #include "mpiech/pph_hash_map.h"
 #include "mpiech/grouping_functions.h"
 
-size_t get_trees_pph(network_abstraction& network, int delta, int load) {
+size_t get_trees_count(network_abstraction& network, int delta, int load,
+	grouping_func_t grouping_func, algorithm_func_t algorithm_func) {
 
 	size_t trees = 0;
 	while (network_helper::is_connected(network)) {
 
 		size_t multicast_nodes_count = 80;
 		std::vector<size_t> multicast_nodes;
-		grouping::high_degree(network, multicast_nodes_count, multicast_nodes);
+		grouping_func(network, multicast_nodes_count, multicast_nodes);
 
 		network_path path;
-		int cost = PPH_HM(network, multicast_nodes, network.get_nodes_count(),
-			multicast_nodes.size(), delta, path);
+		int cost = algorithm_func(network, multicast_nodes, delta, path);
 
 		if (cost < 999999999) {
 			network_helper::increase_costs_due_to_transmission_load(network,
@@ -56,7 +64,9 @@ int main() {
 
 		for (size_t j = 0; j < repetitions_per_network; ++j) {
 			network_abstraction network_copy(network);
-			size_t trees = get_trees_pph(network_copy, delta, load);
+			size_t trees = get_trees_count(network_copy, delta, load,
+				grouping_func_t(&grouping::high_degree),
+				algorithm_func_t(&PPH_HM));
 			results.insert_result("PPH", i, trees);
 		}
 	}
